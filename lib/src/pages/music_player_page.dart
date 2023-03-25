307a1827877463a3696beb878c1d41bfb3b5d46e
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-25 11:34:37 -0600

    178. Imagen del disco

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -8,9 +8,74 @@ class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(children: [
-        CustomAppBar(),
-      ],),
+      body: Column(
+        children: [
+          CustomAppBar(),
+          ImageDiscDuration(),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageDiscDuration extends StatelessWidget {
+  const ImageDiscDuration({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          _DiscImage(),
+          //TODO Progress bar
+        ],
+      ),
+    );
+  }
+}
+
+class _DiscImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      padding: const EdgeInsets.all(20),
+      width: 250,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Color(0xff484750),
+            Color(0xff1E1C24),
+          ],
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(200),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            const Image(image: AssetImage('assets/aurora.jpg')),
+            Container(
+              height: 25,
+              width: 25,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Colors.black38,
+              ),
+            ),
+            Container(
+              height: 18,
+              width: 18,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: const Color(0xff1C1C25),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
