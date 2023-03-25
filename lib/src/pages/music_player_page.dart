cc7968c53741a1e2bbe2a7b72f2c88a0f259adf2
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-24 18:05:56 -0600

    177. Custom AppBar

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:music_player/src/widgets/widgets.dart';
+
 class MusicPlayerPage extends StatelessWidget {
   const MusicPlayerPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Column(children: [
+        CustomAppBar(),
+      ],),
+    );
   }
 }
